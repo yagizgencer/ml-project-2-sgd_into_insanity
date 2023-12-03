42fd8cee5e256ee3f36897517f46d0edfb07bd4a
Luturtun <genceryagiz@gmail.com>
2023-12-03 18:27:49 +0100

    further ground-breaking work, truly remarkable 2

--- dipy_test.py
+++ dipy_test.py
@@ -2,43 +2,46 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pickle
-
+import random
 from dipy.core.sphere import disperse_charges, HemiSphere
 from dipy.core.gradients import gradient_table
 from dipy.data import get_sphere
 from dipy.sims.voxel import multi_tensor, multi_tensor_odf
 #from dipy.viz import window, actor
 
-n_pts = 64
-theta = np.pi * np.random.rand(n_pts)
-phi = 2 * np.pi * np.random.rand(n_pts)
-hsph_initial = HemiSphere(theta=theta, phi=phi)
+with open('real_data/hardi-scheme.bvec.txt', 'r') as file:
+    lines = file.readlines()
 
-hsph_updated, potential = disperse_charges(hsph_initial, 5000)
+real_bvecs = []
+for line in lines:
+    values = line.split()
+    real_bvecs.append([float(value) for value in values])
+real_bvecs = np.array(real_bvecs).T
 
-vertices = hsph_updated.vertices
-values = np.ones(vertices.shape[0])
 
-bvecs = np.vstack((vertices, vertices))
-bvals = np.hstack((1000 * values, 2500 * values))
+with open('real_data/hardi-scheme.bval.txt', 'r') as file:
+    lines = file.readlines()
 
-bvecs = np.insert(bvecs, (0, bvecs.shape[0]), np.array([0, 0, 0]), axis=0)
-bvals = np.insert(bvals, (0, bvals.shape[0]), 0)
+real_bvals = []
+for line in lines:
+    values = line.split()
+    real_bvals.append([float(value) for value in values])
 
-gtab = gradient_table(bvals, bvecs)
+real_bvals = np.array(real_bvals).reshape(-1)
 
-# Fixed diffusivity parameters 
-d_parallel = 0.0015#1.7
-d_perp = 0.0003#0.2
+gtab = gradient_table(real_bvals, real_bvecs)
+d_parallel = 0.0017
+d_perp = 0.0002
 eigenvals = [d_parallel, d_perp, d_perp]
 
 # Generate n pairs of angles 
 n = 180
-H = np.zeros((n, len(bvals)))
-H_noisy = np.zeros((n, len(bvals)))
+H_noisy_input = np.zeros((n, len(real_bvals)))
+H_noisy_validation = np.zeros((n, len(real_bvals)))
 thetas = []
 phis = []
 np.random.seed(2)
+
 for i in range(n):
     thetas.append(np.random.uniform(0,90))
     phis.append(np.random.uniform(0,360))
@@ -49,8 +52,8 @@ hemisphere, _ = disperse_charges(hemisphere,50000)
 angle_pairs = []
 for i in range(len(hemisphere.phi)):
     angle_pairs.append((hemisphere.theta[i] * (180/np.pi), hemisphere.phi[i] * (180/np.pi)))
-    H[i] = multi_tensor(gtab, mevals = [eigenvals], S0=100, angles = [angle_pairs[i]], fractions = [100], snr = None)[0]
-    H_noisy[i] = multi_tensor(gtab, mevals = [eigenvals], S0=100, angles = [angle_pairs[i]], fractions = [100], snr = 20)[0]
+    H_noisy_input[i] = multi_tensor(gtab, mevals = [eigenvals], S0=100, angles=[angle_pairs[i]], fractions=[100], snr=random.randint(10, 30))[0]
+    H_noisy_validation[i] = multi_tensor(gtab, mevals = [eigenvals], S0=100, angles=[angle_pairs[i]], fractions=[100], snr=random.randint(10, 30))[0]
 
 
 N = int(1e5)
@@ -58,7 +61,7 @@ N = int(1e5)
 Nf = np.random.randint(3, size=N) + 1
 
 # Creating S and our ground-truth
-S = np.zeros((N, len(bvals)))
+S = np.zeros((N, len(real_bvals)))
 F = np.zeros((N,n))
 for i, k in enumerate(Nf):
     indices = np.random.randint(n, size=k)
@@ -68,20 +71,17 @@ for i, k in enumerate(Nf):
     random_nums /= np.sum(random_nums)
     Nf[i] = np.sum(random_nums >= 0)
     for j in range(k):
-        S[i] += random_nums[j] * H[indices[j]]
+        S[i] += random_nums[j] * H_noisy_input[indices[j]]
         F[i][indices[j]] = random_nums[j]
 
-print(np.shape(S))
-print(np.shape(H))
-print(np.shape(F))
 
-np.save("S.npy",S)
-np.save("F.npy",F)
-np.save("H.npy",H)
-np.save("H_noisy.npy",H_noisy)
+np.save("synthetic_data/S.npy",S)
+np.save("synthetic_data/F.npy",F)
+np.save("synthetic_data/H_noisy_input.npy",H_noisy_input)
+np.save("synthetic_data/H_noisy_validation.npy",H_noisy_validation)
 
-with open('angle_pairs.pkl', 'wb') as file:
+with open('synthetic_data/angle_pairs.pkl', 'wb') as file:
     pickle.dump(angle_pairs, file)
 
-with open('Nf.pkl', 'wb') as file:
+with open('synthetic_data/Nf.pkl', 'wb') as file:
     pickle.dump(Nf, file)
